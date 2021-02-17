3d20ae78ed0cbf6c8a159f30a084edc5497a5996
zr <247076126@qq.com>
2021-02-17 14:10:34 +0800

    gogo

--- src/pages/Detail/components/header.js
+++ src/pages/Detail/components/header.js
@@ -1,8 +1,21 @@
-import { memo } from "react";
+import { memo, useEffect } from "react";
+import { render ,unmountComponentAtNode} from "react-dom";
 import { useSelector, shallowEqual } from "react-redux";
-const Header = () => {
-    let detail =  useSelector((state) => state.getIn(["detail", "detail"]), shallowEqual)
 
+function XX () {
+    return <div>666666666</div>
+}
+let div = document.createElement("div");
+document.body.append(div);
+const Header = () => {
+    let detail = useSelector((state) => state.getIn(["detail", "detail"]), shallowEqual)
+    useEffect(() => {
+        console.log(render(<XX/>, div));
+
+        setTimeout(()=>{
+            console.log(unmountComponentAtNode(div));
+        },2000)
+    }, [])
     return (
         <div >
             55
